a62fa5a0f3c31d4fe98f1999275e7149bee91bf3
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2024-10-27 02:20:26 -0300

    feat: Adds Briography Input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import { SettingsTabs } from '@/components/SettingsTabs';
 import * as Input from '@/components/Input copy';
-import { Mail } from 'lucide-react';
+import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react';
 import * as FileInput from '@/components/Form/FileInput';
 import { Select } from '@/components/Form/Select';
 import { SelectItem } from '@/components/Form/Select/SelectItem';
+import { Textarea } from '@/components/Form/Textarea';
 
 export default function Home() {
   return (
@@ -140,6 +141,65 @@ export default function Home() {
                 Whrite a short introduction.
               </span>
             </label>
+            <div className='space-y-3'>
+              <div className='grid gap-3 grid-cols-2'>
+                <Select placeholder='' defaultValue='normal'>
+                  <SelectItem
+                    value='normal'
+                    defaultChecked
+                    text='Normal Text'
+                  />
+                  <SelectItem value='md' text='Markdown' />
+                </Select>
+
+                <div className='flex items-center gap-1'>
+                  <button
+                    type='button'
+                    className='p-2 hover:bg-zinc-50 rounded-md'
+                  >
+                    <Bold className='h-4 w-4 text-zinc-500' strokeWidth={3} />
+                  </button>
+                  <button
+                    type='button'
+                    className='p-2 hover:bg-zinc-50 rounded-md'
+                  >
+                    <Italic className='h-4 w-4 text-zinc-500' strokeWidth={3} />
+                  </button>
+                  <button
+                    type='button'
+                    className='p-2 hover:bg-zinc-50 rounded-md'
+                  >
+                    <Link className='h-4 w-4 text-zinc-500' strokeWidth={3} />
+                  </button>
+                  <button
+                    type='button'
+                    className='p-2 hover:bg-zinc-50 rounded-md'
+                  >
+                    <Italic className='h-4 w-4 text-zinc-500' strokeWidth={3} />
+                  </button>
+                  <button
+                    type='button'
+                    className='p-2 hover:bg-zinc-50 rounded-md'
+                  >
+                    <List className='h-4 w-44text-zinc-500' strokeWidth={3} />
+                  </button>
+                  <button
+                    type='button'
+                    className='p-2 hover:bg-zinc-50 rounded-md'
+                  >
+                    <ListOrdered
+                      className='h-4 w-4 text-zinc-500'
+                      strokeWidth={3}
+                    />
+                  </button>
+                </div>
+              </div>
+
+              <Textarea
+                id='bio'
+                defaultValue='Im a Product Designer based in Melbourne, Australia. I specialise in UX/UI design, brand strategy, and Webflow development.'
+              />
+            </div>
           </div>
 
           <div className='grid gap-3 grid-cols-form pt-5'>
