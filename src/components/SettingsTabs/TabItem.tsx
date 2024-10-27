a874e274beb31514aec13437a6fb93fa2e5b74ae
Raiane Batista <153535785+RaianeBatista@users.noreply.github.com>
2024-10-27 12:01:06 -0300

    feat: add button variant

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import * as Tabs from '@radix-ui/react-tabs';
-import {motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 export interface TbaItemProps {
   value: string;
@@ -13,12 +13,18 @@ export function TabItem({ value, title, isSelected = false }: TbaItemProps) {
   return (
     <Tabs.Trigger
       value={value}
-      className='relative px-1 pb-4 text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-700'
+      className='relative group px-1 pb-4 text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-700 outline-none'
     >
-      <span>{title}</span>
+      {/* <span className='group-focus-visible:ring-2 group-focus-visible:ring-violet-400 rounded group-focus-visible:ring-offset-4'
+      >{title}</span> */}
+      <span className='group-focus:ring-2 group-focus:ring-violet-400 rounded group-focus:ring-offset-4'
+      >{title}</span>
 
       {isSelected && (
-        <motion.div layoutId='activeTab' className='absolute -bottom-px  left-0 right-0 h-0.5 bg-violet-700' />
+        <motion.div
+          layoutId='activeTab'
+          className='absolute -bottom-px  left-0 right-0 h-0.5 bg-violet-700'
+        />
       )}
     </Tabs.Trigger>
   );
